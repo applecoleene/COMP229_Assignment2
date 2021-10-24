3e5c8ca63d54ba2ff9e816940cdcd022445db439
applecoleene <90641429+applecoleene@users.noreply.github.com>
2021-10-24 18:31:08 -0400

    updated db URI with MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -8,5 +8,6 @@ Date: October 24, 2021
 //connecting to the mongodb contact_list database
 module.exports = 
 {
-    "URI": "mongodb://localhost/contact_list"
+    //"URI": "mongodb://localhost/contact_list"
+    "URI": "mongodb+srv://applecoleene:ANvGmkjjed0oH2VW@mongodbserver.kf6tf.mongodb.net/contact_list?retryWrites=true&w=majority"
 }
